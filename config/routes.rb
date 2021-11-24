4343aee9785ad2beaa52f83b2dbe386d7b37a3a1
Anton Bril <anton.bril.blr@gmail.com>
2021-11-24 23:45:35 +0300

    Created default user model

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   scope '(:locale)', locale: /en|ru|by/ do
+    devise_for :users
     resources :reviews
+    
+    get 'persons/profile', as: 'user_root'
 
     root to: 'reviews#index'
   end
