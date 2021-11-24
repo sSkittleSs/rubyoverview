7a5102864e65cd0c5f719bfa2472a8078dd82d51
Anton Bril <anton.bril.blr@gmail.com>
2021-11-24 23:01:50 +0300

    Added pagination for review cards

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,2 +1,3 @@
 class Review < ApplicationRecord
+    self.per_page = 30
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+100.times do |index|
+  Review.create!(
+    name: "Обзор на кино ##{index}",
+    group: "Кино",
+    content: "Текст обзора ##{index}"
+  )
+end
+
+100.times do |index|
+  Review.create!(
+    name: "Обзор на книгу ##{index}",
+    group: "Книга",
+    content: "Текст обзора ##{index}"
+  )
+end
