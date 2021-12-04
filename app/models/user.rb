57b5a87f3f3e3973a0e8b41125862a042ef5059f
Anton Bril <anton.bril.blr@gmail.com>
2021-12-04 04:41:53 +0300

    Added base role assign for users after registration

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,20 +1,26 @@
 class User < ApplicationRecord
   USERS_PER_PAGE = 30
   self.per_page = USERS_PER_PAGE
-
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
-
+  :recoverable, :rememberable, :validatable
+  
   has_many :reviews, dependent: :destroy
   has_many :ratings, dependent: :destroy
   has_many :user_roles, dependent: :destroy
   has_many :roles, through: :user_roles
+  
+  after_create :assign_user_role
 
   validates :username, :email, presence: true
 
   def role? role
     roles&.include? Role.find_role(role)
+  end
+
+  private
+  
+  def assign_user_role
+    roles&.push Role.find_role(:user) unless role?(:user)
   end
 end
--- db/migrate/20211203221646_add_index_to_user_roles.rb
+++
@@ -1,5 +0,0 @@
-class AddIndexToUserRoles < ActiveRecord::Migration[6.1]
-  def change
-    add_index :user_roles, [:user_id, :role_id], unique: true
-  end
-end
